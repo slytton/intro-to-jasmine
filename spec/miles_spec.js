f096c5f05081d40fe4fc364c6c89c84798c90b2d
Seth Lytton <slytton@gmail.com>
2016-02-24 11:53:43 -0700

    initial testing for hasEnoughGas

--- spec/miles_spec.js
+++ spec/miles_spec.js
@@ -1,7 +1,22 @@
 var miles = require('../lib/miles')
 
 describe('miles', function(){
-  it('is our master plan here', function(){
-    expect(miles.someFunction()).toEqual('running stuff fo-real')
+  it('determines if a car has enough gas to travel a route', function(){
+    var car = {
+      milesPerGallon: 20,
+      gallonsInTank: 3
+    }
+    var route = {
+      miles: 200,
+      destination: "Denver",
+      location: "Fancy Land"
+    }
+    expect(miles.hasEnoughGas(car, route)).toEqual(false)
+    car.gallonsInTank = 20;
+    expect(miles.hasEnoughGas(car, route)).toEqual(true)
+    car.gallonsInTank = 10;
+    expect(miles.hasEnoughGas(car, route)).toEqual(true)
+    car.gallonsInTank = 9.5;
+    expect(miles.hasEnoughGas(car, route)).toEqual(false)
   });
 })
