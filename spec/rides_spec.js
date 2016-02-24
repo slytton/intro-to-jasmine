f5a4f882f8e0658f52bd846ce24bdbe84dd8a52a
Seth Lytton <slytton@gmail.com>
2016-02-24 13:18:00 -0700

    Complete ride tests and functionality

--- spec/rides_spec.js
+++ spec/rides_spec.js
@@ -1,29 +1,36 @@
 var rides = require("../lib/rides.js")
 
 describe('Rides', function(){
-  it('determines if the child is tall enough to go on the ride', function(){
-    var child1 = {
-      height: 3,
-      name: "Julian",
-      age:3
-    };
 
-    var child2 = {
-      height: 4,
-      name: "Cero",
-      age: 4
-    };
+  var child1 = {
+    height: 3,
+    name: "Julian",
+    age:3
+  };
 
-    var ride1 = {
-      minHeight: 3,
-      maxHeight: 7
-    };
+  var child2 = {
+    height: 4,
+    name: "Cero",
+    age: 4
+  };
 
-    var ride2 = {
-      minHeight: 4,
-      maxHeight: 7
-    };
+  var ride1 = {
+    minHeight: 3,
+    maxHeight: 7
+  };
 
-    expect(rides.isTallEnough()).toEqual(true);
+  var ride2 = {
+    minHeight: 4,
+    maxHeight: 7
+  };
+
+  it('determines if the child is too short', function(){
+    expect(rides.isTallEnough(child1, ride2)).toEqual(false);
+  });
+
+  it('determines if the child is tall enough', function(){
+    expect(rides.isTallEnough(child1, ride1)).toEqual(true);
+    expect(rides.isTallEnough(child2, ride1)).toEqual(true);
+    expect(rides.isTallEnough(child2, ride2)).toEqual(true);
   });
 });
